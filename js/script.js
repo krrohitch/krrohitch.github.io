db558c547d82c2c90f4fd90b6f63b04ed9d3798e
krrohitch <krrohitch@gmail.com>
2019-07-15 01:32:43 +0530

    Loading animation added + button fixed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
+// Preloader
+var timer;
+
+function preloader() {
+	timer = setTimeout(showPage, 2000);
+}
+function showPage() {
+  document.getElementById("preloader").style.display = "none";
+  document.getElementById("container").style.display = "block";
+}
+
 // Smooth scroll
 $('.smoothScroll').click(function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            $('html,body').animate({
-                scrollTop: target.offset().top + 120
-            }, 800);
-            return false;
-        }
-    }
+	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		if (target.length) {
+			$('html,body').animate({
+				scrollTop: target.offset().top + 120
+			}, 800);
+			return false;
+		}
+	}
 });
