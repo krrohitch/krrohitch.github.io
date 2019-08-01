c8ba8e604da46a94a1d302669b155e74565a8393
krrohitch <krrohitch@gmail.com>
2019-08-02 00:48:17 +0530

    Added comments + meta

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var timer;
 function preloader() {
 	timer = setTimeout(showPage, 2000);
 }
+
 function showPage() {
   document.getElementById("preloader").style.display = "none";
   document.getElementById("container").style.display = "block";
@@ -11,14 +12,30 @@ function showPage() {
 
 // Smooth scroll
 $('.smoothScroll').click(function () {
-	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-		var target = $(this.hash);
-		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-		if (target.length) {
-			$('html,body').animate({
-				scrollTop: target.offset().top + 120
-			}, 800);
-			return false;
-		}
-	}
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top + 120
+      }, 800);
+    return false;
+    }
+  }
 });
+
+// Scroll to top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 4000 || document.documentElement.scrollTop > 4000) {
+    document.getElementById("top").style.display = "block";
+  } else {
+    document.getElementById("top").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
